e11598bf1079680487e74d43316d124b28c565df
Andre Alao <andreffalao@gmail.com>
2023-09-14 09:18:03 +0100

    test 1.1

--- app/src/main/java/com/cap/authenticationservice/AuthenticationServiceApplication.java
+++ app/src/main/java/com/cap/authenticationservice/AuthenticationServiceApplication.java
@@ -1,5 +1,8 @@
 package com.cap.authenticationservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class AuthenticationServiceApplication {
 
